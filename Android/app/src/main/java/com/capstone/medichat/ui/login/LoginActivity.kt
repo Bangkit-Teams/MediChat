75815e1d6093c15e3cf61c76fb64d4bd636e3af8
Zacklory(Fiqri) <fiqrifebriansyahmuhamad@gmail.com>
2024-06-01 16:12:36 +0700

    Update LoginActivity.kt

--- Android/app/src/main/java/com/capstone/medichat/ui/login/LoginActivity.kt
+++ Android/app/src/main/java/com/capstone/medichat/ui/login/LoginActivity.kt
@@ -1,11 +1,14 @@
 package com.capstone.medichat.ui.login
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.capstone.medichat.R
+import com.capstone.medichat.ui.main.MainActivity
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,6 +16,14 @@ class LoginActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_login)
 
+        val loginButton: Button = findViewById(R.id.loginButton)
 
+        loginButton.setOnClickListener {
+
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+
+            finish()
+        }
     }
 }
