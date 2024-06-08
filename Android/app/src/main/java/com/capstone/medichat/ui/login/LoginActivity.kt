e9ec5553bd6536879f43df5e0ca3cf01f270cbc9
HasbiAssiddiq <mhasbiraz@gmail.com>
2024-06-08 16:15:52 +0700

    ui

--- Android/app/src/main/java/com/capstone/medichat/ui/login/LoginActivity.kt
+++ Android/app/src/main/java/com/capstone/medichat/ui/login/LoginActivity.kt
@@ -12,6 +12,7 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        supportActionBar?.hide()
         setContentView(R.layout.activity_login)
 
         val loginButton: Button = findViewById(R.id.loginButton)
--- Android/app/src/main/java/com/capstone/medichat/ui/register/RegisterActivity.kt
+++ Android/app/src/main/java/com/capstone/medichat/ui/register/RegisterActivity.kt
@@ -12,6 +12,7 @@ class RegisterActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_register)
+        supportActionBar?.hide()
 
     }
 }
